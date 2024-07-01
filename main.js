05604035c1762b2e85feab9c68d24fa495248441
johnnyb-2010 <noenoenoe.nein@gmail.com>
2024-07-01 18:31:06 +0200

    .

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const taskInput = document.getElementById('input-to-do');
-const list = document.getElementById('tasklist');
-const button = document.getElementById('button');
+document.addEventListener('DOMContentLoaded', function() {
+
+    const itemInput = document.getElementById('input-to-do');
+    const list = document.getElementById('tasklist');
+    const button = document.getElementById('addbutton');
+    button.addEventListener('click', function() {
+
+        const taskText = itemInput.value.trim();
+        if (taskText !== '') {
+            const addItem = document.createElement('li');
+            addItem.textContent = taskText;
+            list.appendChild(addItem);
+            itemInput.value = '';
+        }
+    });
+});
